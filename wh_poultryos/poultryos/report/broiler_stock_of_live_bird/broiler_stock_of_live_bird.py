c06b170a498055bd768b5cabda785198a0e08aa3
Manya2828 <121924237+Manya2828@users.noreply.github.com>
2025-03-06 20:01:12 +0530

    change in report deviation report

--- wh_poultryos/poultryos/report/broiler_stock_of_live_bird/broiler_stock_of_live_bird.py
+++ wh_poultryos/poultryos/report/broiler_stock_of_live_bird/broiler_stock_of_live_bird.py
@@ -11,12 +11,14 @@ def execute(filters=None):
         SELECT 
             b1.batch_name,
             b1.farmer_name,
-            b1.shed,
-            b1.opening_date,
+            b1.opening_date as "Placement Date",
             b1.live_batch_date,
             b1.batch_age_in_days AS Age,
             b1.place_quantity_number_of_birds AS Placement_Quantity,
             b1.live_quantity_number_of_birds AS Live_Quantity,
+            b1.mortality,
+            b1.culls,
+            b1.body_weight,
             b1.rate,
             b1.amount,
             b1.batch_status
@@ -38,17 +40,19 @@ def execute(filters=None):
 
     # Define columns for report
     columns = [
-        {"label": "Batch Name", "fieldname": "batch_name", "fieldtype": "Data", "width": 150},
-        {"label": "Farmer Name", "fieldname": "farmer_name", "fieldtype": "Data", "width": 150},
-        {"label": "Shed", "fieldname": "shed", "fieldtype": "Data", "width": 120},
-        {"label": "Opening Date", "fieldname": "opening_date", "fieldtype": "Date", "width": 120},
-        {"label": "Live Batch Date", "fieldname": "live_batch_date", "fieldtype": "Date", "width": 120},
-        {"label": "Age (Days)", "fieldname": "Age", "fieldtype": "Int", "width": 80},
-        {"label": "Placement Quantity", "fieldname": "Placement_Quantity", "fieldtype": "Int", "width": 120},
-        {"label": "Live Quantity", "fieldname": "Live_Quantity", "fieldtype": "Int", "width": 120},
-        {"label": "Rate", "fieldname": "rate", "fieldtype": "Currency", "width": 100},
-        {"label": "Amount", "fieldname": "amount", "fieldtype": "Currency", "width": 120},
         {"label": "Batch Status", "fieldname": "batch_status", "fieldtype": "Data", "width": 120},
+        {"label": "Batch Name", "fieldname": "batch_name", "fieldtype": "Data", "width": 130},
+        {"label": "Farmer Name", "fieldname": "farmer_name", "fieldtype": "Data", "width": 115},      
+        {"label": "Placement Date", "fieldname": "Placement Date", "fieldtype": "Date", "width": 130},
+        {"label": "Live Batch Date", "fieldname": "live_batch_date", "fieldtype": "Date", "width": 140},
+        {"label": "Place Quantity", "fieldname": "Placement_Quantity", "fieldtype": "Int", "width": 130},
+        {"label": "Live Quantity", "fieldname": "Live_Quantity", "fieldtype": "Int", "width": 120},
+        {"label": "Mortality", "fieldname": "mortality", "fieldtype": "Int", "width": 90},
+        {"label": "Culls", "fieldname": "culls", "fieldtype": "Int", "width": 70},
+        {"label": "Body Weight", "fieldname": "body_weight", "fieldtype": "float", "width": 110},
+        {"label": "Rate", "fieldname": "rate", "fieldtype": "Currency", "width": 100},
+        {"label": "Amount", "fieldname": "amount", "fieldtype": "Currency", "width": 120}
+       
     ]
 
     return columns, data
